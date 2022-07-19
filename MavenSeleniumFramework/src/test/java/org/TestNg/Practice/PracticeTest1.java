51ee238da89f5f6cc2876c866ffdc3257304a56e
SweetiBassi <103253489+SweetiBassi@users.noreply.github.com>
2022-07-19 15:50:49 +0530

    Update PracticeTest1.java

--- MavenSeleniumFramework/src/test/java/org/TestNg/Practice/PracticeTest1.java
+++ MavenSeleniumFramework/src/test/java/org/TestNg/Practice/PracticeTest1.java
@@ -30,7 +30,5 @@ System.out.println("Password------->"+password);
 public void display1Test()
 {
 Reporter.log("Executing Test2" , true);
-System.out.println("Pass");
-System.out.println("Fail");
-System.out.println("yes");
+
 }}
