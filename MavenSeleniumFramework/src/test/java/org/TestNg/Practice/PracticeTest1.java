c5f2fbb7af2fde2f5a9d5f078e32a0cadb0cac7b
dell <dell@AUTOMATION>
2022-07-19 11:43:53 +0530

    1st Commit

---
+++ MavenSeleniumFramework/src/test/java/org/TestNg/Practice/PracticeTest1.java
@@ -0,0 +1,33 @@
+package org.TestNg.Practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeTest1 {
+
+@Test
+
+public void displayTest()
+{
+	String url = System.getProperty("URL");
+	String browser=	System.getProperty("BROWSER");
+	String username=System.getProperty("USERNAME");
+	String password=System.getProperty("PASSWORD");
+Reporter.log("Executing Test1" , true);
+System.out.println("Url------->"+url);
+System.out.println("browser------->"+browser);
+System.out.println("Username------->"+username);
+
+System.out.println("Password------->"+password);
+
+
+
+}
+@Test
+public void display1Test()
+{
+Reporter.log("Executing Test2" , true);
+}}
---
+++ MavenSeleniumFramework/src/test/java/org/TestNg/Practice/PracticeTest2.java
@@ -0,0 +1,22 @@
+package org.TestNg.Practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeTest2 {
+	@Test
+	
+	public void display()
+	{
+	Reporter.log("Executing Test1" , true);
+	}
+	@Test
+	
+	public void display1()
+	{
+	Reporter.log("Executing Test2" , true);
+	}
+}
