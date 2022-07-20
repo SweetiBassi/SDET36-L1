faa872426464657fb1b21847d42c994830b1151b
dell <dell@AUTOMATION>
2022-07-20 12:33:11 +0530

    1

---
+++ MavenSeleniumFramework/src/test/java/org/TestNg/Practice/PracticeTest3.java
@@ -0,0 +1,21 @@
+package org.TestNg.Practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest3 {
+	@Test
+	public void Test1() {
+		System.out.println("Test1 -> class3");
+		
+	}
+	@Test
+	public void Test2() {
+		System.out.println("Test2 -> class3");
+		
+	}
+	@Test
+	public void Test3() {
+		System.out.println("Test3 -> class3");
+		
+	}
+}
